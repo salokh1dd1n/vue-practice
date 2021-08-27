2469c4dc319bf4bbd93ed178e898d36db3c99b38
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-27 14:02:31 +0500

    HomeWork #1 Done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <MyComponent />
+    <HomeWork />
   </div>
 </template>
 
 <script>
 import MyComponent from "./components/MyComponent";
-
+import HomeWork from "./components/HomeWork";
 export default {
   name: "App",
   components: {
     MyComponent,
+    HomeWork,
   },
 };
 </script>
---
+++ src/components/HomeWork.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="root">
+    <button @click="minusButton"><strong>-</strong></button>
+    <input type="number" :value="value" />
+    <button @click="plusButton"><strong>+</strong></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeWork",
+  data() {
+    return {
+      value: 1,
+    };
+  },
+  methods: {
+    minusButton() {
+      if (this.value === 0) return;
+      this.value -= 1;
+    },
+    plusButton() {
+      if (this.value === 10) return;
+      this.value += 1;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
