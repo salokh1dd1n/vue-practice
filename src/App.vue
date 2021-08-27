7d6e8b48e7f36103f2c795a799482e31c6434b3a
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-27 13:57:24 +0500

    Updated My Component and App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-
+    <MyComponent />
   </div>
 </template>
 
 <script>
+import MyComponent from "./components/MyComponent";
 
 export default {
   name: "App",
   components: {
+    MyComponent,
   },
 };
 </script>
-
--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="root">
-
+    <label for="email">Email:</label>
+    <input
+      type="email"
+      id="email"
+      placeholder="Enter Email"
+      :value="email"
+      @input="emailInput($event.target.value)"
+    />
+    <label for="password">Password:</label>
+    <input
+      type="password"
+      id="password"
+      placeholder="Enter password"
+      :value="password"
+      @input="passwordInput($event.target.value)"
+    />
+    <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "MyComponent",
+  data() {
+    return {
+      email: "admin@gmail.com",
+      password: "password",
+    };
+  },
+  methods: {
+    emailInput(value) {
+      this.email = value;
+    },
+    passwordInput(value) {
+      this.password = value;
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
