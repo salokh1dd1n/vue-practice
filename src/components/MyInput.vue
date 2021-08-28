6299b67d2c0b74494e79efdc37b8e5462d5a82ff
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-28 23:08:15 +0500

    Created MyInput.vue component

---
+++ src/components/MyInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Search for model..." />
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+      Search
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyInput",
+};
+</script>
+
+<style scoped></style>
