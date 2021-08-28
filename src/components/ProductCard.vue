6061037837ad959bfb3ca3a16675f54f5c178c5a
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-28 22:42:53 +0500

    Updated ProductCard.vue used "props"

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="container">
-    <p class="display-5 text-center my-3">List of Cars</p>
-    <hr />
-    <div class="card" style="width: 18rem">
-      <img :src="imgUrl" class="card-img-top" alt="bla" />
-      <div class="card-body">
-        <h5 class="card-title">Model: {{ title }}</h5>
-        <p class="card-text">Price: {{ price }}$</p>
-        <p class="card-text">Amount: {{ amount }}</p>
-        <a href="#" class="btn btn-primary w-100">Book</a>
-      </div>
+  <div class="card my-3">
+    <img :src="imgUrl" class="card-img-top" alt="bla" />
+    <div class="card-body">
+      <h5 class="card-title">Model: {{ title }}</h5>
+      <p class="card-text">Price: {{ price }}$</p>
+      <p class="card-text">Amount: {{ amount }}</p>
+      <a href="#" class="btn btn-primary w-100">Book</a>
     </div>
   </div>
 </template>
@@ -17,14 +13,7 @@
 <script>
 export default {
   name: "ProductCard",
-  data() {
-    return {
-      title: "Chevrolet Spark",
-      price: 5000,
-      imgUrl: "https://picsum.photos/500/500",
-      amount: 20,
-    };
-  },
+  props: ["title", "price", "imgUrl", "amount"],
 };
 </script>
 
