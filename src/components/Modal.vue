8f13a85d31831f7f201f93e2cf6a6744efc86fd7
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-29 23:56:36 +0500

    Updated Modal.vue: added logic

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,11 +3,11 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title">{{ title }}</h5>
           <button
             type="button"
             class="btn-close"
-            data-bs-dismiss="modal"
+            @close="closeModal"
             aria-label="Close"
           ></button>
         </div>
@@ -15,11 +15,7 @@
           <p>Modal body text goes here.</p>
         </div>
         <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
+          <button type="button" class="btn btn-secondary" @close="closeModal">
             Close
           </button>
           <button type="button" class="btn btn-primary">Save changes</button>
@@ -32,6 +28,17 @@
 <script>
 export default {
   name: "Modal",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
 };
 </script>
 
