494fcbf77e900a0f803a05ca5de6f33b61ce48d7
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-29 23:48:59 +0500

    Updated Modal.vue: imported and connected Modal.vue component to UserPage.vue

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,10 +1,14 @@
 <template>
   <button type="button" class="btn btn-outline-primary">Primary</button>
+  <modal />
 </template>
 
 <script>
+import Modal from "./Modal";
+
 export default {
   name: "UserPage",
+  components: { Modal },
 };
 </script>
 
