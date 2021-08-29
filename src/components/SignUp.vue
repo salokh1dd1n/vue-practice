adb56b874800b2fa77348ca2c108bb42bbc05d7c
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-29 19:26:26 +0500

    Updated SignUp.vue component: worked with form (input, select, checkbox, radio)

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,36 +3,54 @@
     <h3 class="mt-3 text-center">Sign Up</h3>
     <div class="mb-3">
       <label for="login" class="form-label">Login:</label>
-      <input type="email" class="form-control" id="login" />
+      <input type="email" class="form-control" id="login" v-model="login" />
     </div>
     <div class="mb-3">
       <label for="email" class="form-label">Email address:</label>
-      <input type="email" class="form-control" id="email" />
+      <input type="email" class="form-control" id="email" v-model="email" />
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Password:</label>
-      <input type="password" class="form-control" id="password" />
+      <input
+        type="password"
+        class="form-control"
+        id="password"
+        v-model="password"
+      />
     </div>
     <div class="mb-3">
       <label class="form-label">Country of residence:</label>
-      <select class="form-select">
-        <option value="1">Uzbekistan</option>
-        <option value="2">Poland</option>
-        <option value="3">USA</option>
+      <select class="form-select" v-model="country">
+        <option
+          :value="country.value"
+          v-for="(country, index) in countries"
+          :key="index"
+        >
+          {{ country.label }}
+        </option>
       </select>
     </div>
     <div class="mb-3">
       <label class="form-label">Field:</label>
-      <select class="form-select">
-        <option value="1">IT</option>
-        <option value="2">SMM</option>
-        <option value="3">Logistics</option>
+      <select class="form-select" v-model="field" multiple>
+        <option
+          :value="field.value"
+          v-for="(field, index) in fields"
+          :key="index"
+        >
+          {{ field.label }}
+        </option>
       </select>
     </div>
     <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-      <label class="form-check-label" for="exampleCheck1">
-        Notify me about courses
+      <input
+        type="checkbox"
+        class="form-check-input"
+        id="sendEmail"
+        v-model="agreeWithSendEmail"
+      />
+      <label class="form-check-label" for="sendEmail">
+        Send emails about new courses
       </label>
     </div>
     <div class="mb-3">
@@ -40,30 +58,69 @@
         <input
           class="form-check-input"
           type="radio"
-          name="inlineRadioOptions"
-          id="inlineRadio1"
-          value="option1"
+          id="male"
+          value="male"
+          v-model="gender"
         />
-        <label class="form-check-label" for="inlineRadio1">Male</label>
+        <label class="form-check-label" for="male">Male</label>
       </div>
       <div class="form-check form-check-inline">
         <input
           class="form-check-input"
           type="radio"
-          name="inlineRadioOptions"
-          id="inlineRadio2"
-          value="option2"
+          id="female"
+          value="female"
+          v-model="gender"
         />
-        <label class="form-check-label" for="inlineRadio2">Female</label>
+        <label class="form-check-label" for="female">Female</label>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary">Save</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "SignUp",
+  data() {
+    return {
+      login: "",
+      email: "",
+      password: "",
+      field: "IT",
+      country: "uzbekistan",
+      agreeWithSendEmail: false,
+      gender: "male",
+      fields: [
+        {
+          label: "Social Media Marketing",
+          value: "SMM",
+        },
+        {
+          label: "Information Technology",
+          value: "IT",
+        },
+        {
+          label: "Search Engine Optimization",
+          value: "SEO",
+        },
+      ],
+      countries: [
+        {
+          label: "Uzbekistan",
+          value: "uzbekistan",
+        },
+        {
+          label: "Poland",
+          value: "poland",
+        },
+        {
+          label: "USA",
+          value: "usa",
+        },
+      ],
+    };
+  },
 };
 </script>
 
