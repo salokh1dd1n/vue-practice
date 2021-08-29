61a2422a9d28fcec64b2d3d13519fb900188e153
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-29 23:29:28 +0500

    Updated SignUp.vue component: used Vuelidate which is not compatible with vue 3

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,96 +1,119 @@
 <template>
   <div class="container">
     <h3 class="mt-3 text-center">Sign Up</h3>
-    <div class="mb-3">
-      <label for="login" class="form-label">Login:</label>
-      <input type="email" class="form-control" id="login" v-model="login" />
-    </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">Email address:</label>
-      <input type="email" class="form-control" id="email" v-model="email" />
-    </div>
-    <div class="mb-3">
-      <label for="password" class="form-label">Password:</label>
-      <input
-        type="password"
-        class="form-control"
-        id="password"
-        v-model="password"
-      />
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Country of residence:</label>
-      <select class="form-select" v-model="country">
-        <option
-          :value="country.value"
-          v-for="(country, index) in countries"
-          :key="index"
-        >
-          {{ country.label }}
-        </option>
-      </select>
-    </div>
-    <div class="mb-3">
-      <label class="form-label">Field:</label>
-      <select class="form-select" v-model="field" multiple>
-        <option
-          :value="field.value"
-          v-for="(field, index) in fields"
-          :key="index"
-        >
-          {{ field.label }}
-        </option>
-      </select>
-    </div>
-    <div class="mb-3 form-check">
-      <input
-        type="checkbox"
-        class="form-check-input"
-        id="sendEmail"
-        v-model="agreeWithSendEmail"
-      />
-      <label class="form-check-label" for="sendEmail">
-        Send emails about new courses
-      </label>
-    </div>
-    <div class="mb-3">
-      <div class="form-check form-check-inline">
+    <form @submit.prevent="onSubmit">
+      <div class="mb-3">
+        <label for="login" class="form-label">Login:</label>
         <input
-          class="form-check-input"
-          type="radio"
-          id="male"
-          value="male"
-          v-model="gender"
+          :class="{ error"
+          type="email"
+          class="form-control"
+          id="login"
+          v-model="form.login"
         />
-        <label class="form-check-label" for="male">Male</label>
       </div>
-      <div class="form-check form-check-inline">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address:</label>
         <input
-          class="form-check-input"
-          type="radio"
-          id="female"
-          value="female"
-          v-model="gender"
+          type="email"
+          class="form-control"
+          id="email"
+          v-model="form.email"
         />
-        <label class="form-check-label" for="female">Female</label>
       </div>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password:</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          v-model="form.password"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Country of residence:</label>
+        <select class="form-select" v-model="form.country">
+          <option
+            :value="country.value"
+            v-for="(country, index) in countries"
+            :key="index"
+          >
+            {{ country.label }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Field:</label>
+        <select class="form-select" v-model="form.field" multiple>
+          <option
+            :value="field.value"
+            v-for="(field, index) in fields"
+            :key="index"
+          >
+            {{ field.label }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-3 form-check">
+        <input
+          type="checkbox"
+          class="form-check-input"
+          id="sendEmail"
+          v-model="form.agreeWithSendEmail"
+        />
+        <label class="form-check-label" for="sendEmail">
+          Send emails about new courses
+        </label>
+      </div>
+      <div class="mb-3">
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            id="male"
+            value="male"
+            v-model="form.gender"
+          />
+          <label class="form-check-label" for="male">Male</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="radio"
+            id="female"
+            value="female"
+            v-model="form.gender"
+          />
+          <label class="form-check-label" for="female">Female</label>
+        </div>
+      </div>
+      <button :disabled="v$.form.invalid" type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
+import useVuelidate from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+
 export default {
   name: "SignUp",
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
-      login: "",
-      email: "",
-      password: "",
-      field: "IT",
-      country: "uzbekistan",
-      agreeWithSendEmail: false,
-      gender: "male",
+      form: {
+        login: "",
+        email: "",
+        password: "",
+        field: "IT",
+        country: "uzbekistan",
+        agreeWithSendEmail: false,
+        gender: "male",
+      },
       fields: [
         {
           label: "Social Media Marketing",
@@ -119,6 +142,19 @@ export default {
           value: "usa",
         },
       ],
+    };
+  },
+  validations() {
+    return {
+      form: {
+        login: {
+          required,
+          simpleValidator(value) {
+            console.log(value);
+            return value.length > 5;
+          },
+        },
+      },
     };
   },
 };
