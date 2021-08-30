688c91a3ff97e825f2cfa1513b73c5f5383a7ad2
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-30 15:49:11 +0500

    Created UserPage.vue: prepared for #8 lesson

---
+++ src/components/lesson8/UserPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <button type="button" class="btn btn-outline-primary" @click="openModal">
+    Send something
+  </button>
+  <modal
+    v-if="isModalOpen"
+    title="Too important information"
+    @close="closeModal"
+  >
+    <div class="information">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        fugit illum nisi quisquam recusandae? Ad ducimus est mollitia nisi rem
+        reprehenderit sit velit? Ab, beatae minus nostrum obcaecati repellat
+        velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        fugit illum nisi quisquam recusandae? Ad ducimus est mollitia nisi rem
+        reprehenderit sit velit? Ab, beatae minus nostrum obcaecati repellat
+        velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        fugit illum nisi quisquam recusandae? Ad ducimus est mollitia nisi rem
+        reprehenderit sit velit? Ab, beatae minus nostrum obcaecati repellat
+        velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        fugit illum nisi quisquam recusandae? Ad ducimus est mollitia nisi rem
+        reprehenderit sit velit? Ab, beatae minus nostrum obcaecati repellat
+        velit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        fugit illum nisi quisquam recusandae? Ad ducimus est mollitia nisi rem
+        reprehenderit sit velit? Ab, beatae minus nostrum obcaecati repellat
+        velit.
+      </p>
+    </div>
+  </modal>
+</template>
+
+<script>
+import Modal from "./Modal";
+
+export default {
+  name: "UserPage",
+  components: { Modal },
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
