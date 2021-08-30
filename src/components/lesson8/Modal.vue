d2dfce98ca4c1c455681ab9d06e16108b6d51fe7
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-30 15:49:04 +0500

    Created Modal.vue: prepared for #8 lesson

---
+++ src/components/lesson8/Modal.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="modal fade show">
+    <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button
+            type="button"
+            class="btn-close"
+            @click="closeModal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div
+          class="modal-body"
+          style="height: 200px"
+          ref="modalBody"
+          @scroll="onBodyScroll"
+        >
+          <slot></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            <button type="button" class="btn btn-secondary" @click="closeModal">
+              Close
+            </button>
+            <button
+              :disabled="!isScrolledFully"
+              type="button"
+              class="btn btn-primary"
+            >
+              Save changes
+            </button>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      isScrolledFully: false,
+    };
+  },
+  mounted() {
+    let modalBody = this.$refs.modalBody;
+    modalBody.scrollTop = modalBody.scrollHeight - modalBody.clientHeight;
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+    onBodyScroll() {
+      let modalBody = this.$refs.modalBody;
+      if (
+        modalBody.scrollHeight ===
+        modalBody.scrollTop + modalBody.clientHeight
+      ) {
+        this.isScrolledFully = true;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  display: block;
+}
+</style>
