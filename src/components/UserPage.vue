5be234bb5b66728bdc416af2ec1e8db9d4bdf40b
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-30 13:46:29 +0500

    Updated UserPage.vue component: slots default and customs

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -6,7 +6,18 @@
     v-if="isModalOpen"
     title="Too important information"
     @close="closeModal"
-  />
+  >
+    <ul>
+      <li>List #1</li>
+      <li>List #2</li>
+    </ul>
+    <template v-slot:footer>
+      <button type="button" class="btn btn-secondary" @click="closeModal">
+        Close Modal
+      </button>
+      <button type="button" class="btn btn-primary">Submit</button>
+    </template>
+  </modal>
 </template>
 
 <script>
