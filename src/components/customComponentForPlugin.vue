d0f94b3c67f548d54944356318f467d426ba8ffa
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-31 22:24:50 +0500

    Created customComponentForPlugin component: not fully completed

---
+++ src/components/customComponentForPlugin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="header-title">
+      <slot name="title"></slot>
+    </h1>
+  </div>
+  <div>
+    <h1 class="header-title">
+      <slot name="author"></slot>
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
