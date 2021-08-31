ec3cbfd23fb77a8f519b8ee5faf598590b0eb141
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-08-31 22:25:14 +0500

    Updated main.js: used custom plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import simplePlugin from "./plugins/simplePlugin";
+
+createApp(App).use(simplePlugin);
 
 createApp(App).mount("#app");
