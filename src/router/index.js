264040d7283e955e9b8045e2b755658e881afdd9
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-02 21:53:38 +0500

    feat: Added Lesson1 route and configured

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home";
 import About from "../views/About";
+import Lesson1 from "../views/Lesson1";
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     // component: () =>
     //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/lesson1",
+    name: "Lesson1",
+    component: Lesson1,
   },
 ];
 
