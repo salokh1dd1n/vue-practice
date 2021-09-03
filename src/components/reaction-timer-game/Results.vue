bf773f674fc6e0f6152509b72cc09f9aea826703
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-03 23:49:00 +0500

    Updated Results.vue component and added ranks

--- src/components/reaction-timer-game/Results.vue
+++ src/components/reaction-timer-game/Results.vue
@@ -1,11 +1,26 @@
 <template>
   <h3>Scope: {{ score }} ms</h3>
+  <div class="bg-info p-2">{{ rank }}</div>
 </template>
 
 <script>
 export default {
   name: "Results",
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Incredible Reflexes";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace...";
+    }
+  },
 };
 </script>
 
