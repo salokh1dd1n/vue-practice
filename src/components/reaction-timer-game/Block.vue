3db9198ba71b7e0a67190b7581ed6e0217de6fb7
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-03 23:15:09 +0500

    Created Block component for Reaction Timer Game

---
+++ src/components/reaction-timer-game/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-success rounded p-5 mt-3 text-white" v-if="showBlock">
+    Click Me
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+};
+</script>
+
+<style scoped></style>
