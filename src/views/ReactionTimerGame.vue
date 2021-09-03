a55e86d0fdcf46eda039e688dcab0831da09f9b1
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-03 23:34:39 +0500

    Updated ReactionTimerGame.vue component and added new features to game with custom events

--- src/views/ReactionTimerGame.vue
+++ src/views/ReactionTimerGame.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container text-center">
     <h2>Reaction Timer Game</h2>
+    <h3 v-if="showResults">Scope: {{ score }} ms</h3>
     <button class="btn btn-primary" @click="start" :disabled="isPlaying">
       Start
     </button>
-    <block v-if="isPlaying" :delay="delay" />
+    <block v-if="isPlaying" :delay="delay" @end="endGame" />
   </div>
 </template>
 
@@ -18,12 +19,20 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
