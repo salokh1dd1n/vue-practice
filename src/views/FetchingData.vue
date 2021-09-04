f1a68abef2a0cbb8338c92b1de1576ce9c60e167
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-05 04:48:41 +0500

    feat: created FetchingData.vue component in order to work with fetch

---
+++ src/views/FetchingData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container text-center">
+    <div class="card" v-for="job in jobs" :key="job.id">
+      <div class="card-body">
+        <h3>{{ job.title }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FetchingData",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
+
+<style scoped></style>
