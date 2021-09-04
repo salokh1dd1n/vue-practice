a7701d7081ac44610def9d4a2da039fd6c872965
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-04 21:52:02 +0500

    chore: Started to learn Composition API basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
+  <div class="home container text-center">
     <h3>Home Page</h3>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <p>My name is {{ name }} and I am {{ age }} year old</p>
+    <button class="btn btn-primary" @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  setup() {
+    let name = "Salokhiddin";
+    let age = 17;
+    const handleClick = () => {
+      console.log("You clicked me");
+    };
+    return { name, age, handleClick };
+  },
 };
 </script>
