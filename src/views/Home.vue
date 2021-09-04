a26eaded18f0cb9645a4dd0844e49d149dcf49f6
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-04 22:22:15 +0500

    feat: played with refs for understanding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,13 @@
   <div class="home container text-center">
     <h3>Home Page</h3>
     <p ref="p">My name is {{ name }} and I am {{ age }} years old</p>
-    <button class="btn btn-primary" @click="handleClick">Click me</button>
+    <div class="d-flex justify-content-center">
+      <button class="btn btn-primary me-2" @click="handleClick">
+        Click me
+      </button>
+      <button class="btn btn-primary me-2" @click="age++">Add +1 to age</button>
+      <input type="text" class="form-control w-25" v-model="name" />
+    </div>
   </div>
 </template>
 
