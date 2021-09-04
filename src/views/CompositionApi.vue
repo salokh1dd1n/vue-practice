ee1c862f814281065efb59a12cb8b7dc29cd1d3e
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-05 03:56:10 +0500

    created CompositionApi view

---
+++ src/views/CompositionApi.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container text-center">Composition Api</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "CompositionApi",
+  setup() {
+    const posts = ref([
+      {
+        id: 1,
+        title: "Welcome to the blog",
+        body: "Lorem Ipsum",
+      },
+      {
+        id: 2,
+        title: "Top 5 JavaScript tips",
+        body: "Lorem Ipsum",
+      },
+    ]);
+    return { posts };
+  },
+};
+</script>
+
+<style scoped></style>
