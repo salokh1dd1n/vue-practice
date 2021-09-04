dc169c0a39ab1e52a3035e34392d14adbe4f70bb
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-04 22:06:48 +0500

    chore: refs in Composition Api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="home container text-center">
     <h3>Home Page</h3>
-    <p>My name is {{ name }} and I am {{ age }} year old</p>
+    <p ref="p">My name is {{ name }} and I am {{ age }} year old</p>
     <button class="btn btn-primary" @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   setup() {
     let name = "Salokhiddin";
     let age = 17;
+    const p = ref(null);
     const handleClick = () => {
-      console.log("You clicked me");
+      console.log(p);
+      console.log(p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "Hello, World!";
     };
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
