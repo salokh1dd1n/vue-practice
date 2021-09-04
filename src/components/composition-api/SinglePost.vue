e3bf385d2ad6f0de23091eec1c66624962b1eb49
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-05 04:14:21 +0500

    chore: created SinglePost.vue component and used props in setup

---
+++ src/components/composition-api/SinglePost.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>{{ post.title }}</h3>
+  <p>{{ snippet }}</p>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "SinglePost",
+  props: ["post"],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.substring(0, 100) + "...";
+    });
+
+    return { snippet };
+  },
+};
+</script>
+
+<style scoped></style>
