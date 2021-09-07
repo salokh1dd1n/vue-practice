fa292d2202ba6319ae91671a5136b76637f2f882
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-07 10:07:25 +0500

    chore: actions, mutations, state. getters

--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,6 +1,22 @@
 export default {
-  actions: {},
-  mutations: {},
-  state: {},
-  getters: {},
+  actions: {
+    async fetchPosts(ctx) {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const posts = await res.json();
+      ctx.commit("updatePost", posts);
+    },
+  },
+  mutations: {
+    updatePost(state, posts) {
+      state.posts = posts;
+    },
+  },
+  state: {
+    posts: [],
+  },
+  getters: {
+    getPosts(state) {
+      return state.posts;
+    },
+  },
 };
